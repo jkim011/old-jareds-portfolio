cb202b2d4c8de31560feeac3feb5da9e6cb8f4b8
Jared Kim <jaredkim01@yahoo.com>
2023-02-12 15:12:39 -0600

    figured out flex wrap for portfolio

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -5,10 +5,10 @@ import '../components/style/Project.css'
 
 function Project(props) {
   return (
-    <div className="card text-bg-dark display margin border wrap">
-      <a className="wrap " href={props.imageHref} target="_blank" rel="noreferrer">
-        <img src={props.imageSrc} className="card-img border display" alt="..."/>
-        <div className="card-img-overlay text-color ">
+    <div className="card text-bg-dark size margin">
+      <a href={props.imageHref} target="_blank" rel="noreferrer">
+        <img src={props.imageSrc} className="card-img" alt="..."/>
+        <div className="card-img-overlay text-color">
           <h5 className="card-title">{props.imageTitle}</h5>
           <p className="card-text">{props.imageDescription}</p>
         </div>
