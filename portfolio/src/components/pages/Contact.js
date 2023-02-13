6c840a84b3c0842f2f00986c48e7e37f68ee6d2d
Jared Kim <jaredkim01@yahoo.com>
2023-02-13 13:31:28 -0600

    finished contact forms

--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,7 +1,98 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function Resume() {
+import '../style/Contact.css'
+
+import {validateEmail} from '../../utils/helpers';
+
+export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const {target} = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      setErrorMessage("Please enter your name")
+      return;
+    }
+    if (!validateEmail(email)) {
+      setErrorMessage("Please enter a valid email");
+      return;
+    }
+    if (!message) {
+      setErrorMessage("Please enter a message")
+      return;
+    }
+    alert("Message sent!");
+
+    setName('');
+    setEmail('');
+    setMessage('');
+    setErrorMessage('');
+  };
+
   return (
-    <div><p>Put contact form here</p></div>
-  )
+    <div>
+      <h3 className='margins'>Contact Me:</h3>
+      <form>
+        <div className='margins'>
+          <label className='textboxLabel'>Name:</label><br></br>
+          <input
+            className='textbox'
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            type="text"
+          />
+        </div>
+        
+        <div className='margins'>
+          <label className='textboxLabel'>Email:</label><br></br>
+          <input
+            className='textbox'
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+          />
+        </div>
+        
+        <div className='margins'>
+          <label className='textboxLabel'>Message:</label><br></br>
+          <textarea
+            rows={12}
+            className='textbox'
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="textarea"
+          />
+        </div>
+        
+        <button type='button' className='submitBtn' onClick={handleFormSubmit}>Submit</button>
+
+      </form>
+
+      {errorMessage && (
+        <div className='margins'>
+          <p className='errorMessage'>{errorMessage}</p>
+        </div>
+      )}
+
+    </div>
+  );
 }
